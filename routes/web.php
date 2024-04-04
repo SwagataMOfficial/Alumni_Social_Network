2011cc34993276de855584bcca153ca25a3f7e9d
Sk Md Altab <sk.md.altab987@gmail.com>
2024-04-04 22:35:25 +0530

    Authentication System by Altab

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['LoginCheck'])->group(function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+    Route::get('/logout', [UserController::class, 'logout'])->name('auth.logout');
 });
+
+Route::get('/register', [UserController::class, 'register'])->name('register');
+Route::get('/forgot', [UserController::class, 'forgot'])->name('forgot');
+Route::get('/reset_password/{email}/{token}', [UserController::class, 'reset_pass'])->name('Reset_Password');
+
+// Ajax request sent using this route
+Route::post("/register", [UserController::class, 'saveUser'])->name('auth.register');
+
+// Ajax request sent using this route
+Route::post("/login", [UserController::class, 'loginUser'])->name('auth.login');
+Route::post("/forgot", [UserController::class, 'forgotPassword'])->name('auth.forgot');
+Route::post("/resset-password", [UserController::class, 'update_password'])->name('auth.reset');
+
+
+
